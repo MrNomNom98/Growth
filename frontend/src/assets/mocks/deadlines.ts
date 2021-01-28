7b4ff766870b558c96a2391579699157de7c3188
Tobias Bergmeier <tobi@Tobiass-iMac.fritz.box>
2021-01-28 22:25:49 +0100

    added countdown div and configured deadline

--- frontend/src/assets/mocks/deadlines.ts
+++ frontend/src/assets/mocks/deadlines.ts
@@ -1,7 +1,12 @@
 import {Deadline} from '../../app/interfaces/deadline'
-
+//counter muss noch eingefügt werden als attribut
 export const DEADLINES : Deadline[] = [
     {titel: "Deadline Nr. 1",
     date: new Date(),
-    inTime: true}
+    inTime: true},
+    {titel: "Deadline Nr. 2",
+    date: new Date(),
+    inTime: true},
+    
 ]
+
